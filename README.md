e74497862c522407374acad79d5bb7a8a9fcf33a
mireu-san <starmireu@gmail.com>
2023-05-31 17:58:26 +0900

    test of api and analysis complete

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # 작업중입니다 :)
+
+
+##  Known status.
+- To run this repository, run live server extension in vscode.
+(Ctrl + Shift + P and 'live server: open with live server')
+
+## Reference
+- https://platform.openai.com/docs/api-reference/chat
--- index.js
+++ index.js
@@ -3,18 +3,48 @@ const $input = document.querySelector("input");
 const $chatList = document.querySelector("ul");
 
 // openAI API
+// This is temporary key. Document strongly not recommend to leave it like here.
 let url = `https://estsoft-openai-api.jejucodingcamp.workers.dev/`;
 
 // 사용자의 질문
+// This one works as receiver and display it as confirmed. #2
 let question;
 
+// https://community.openai.com/t/the-system-role-how-it-influences-the-chat-behavior/87353
 // 질문과 답변 저장
 let data = [
   {
     role: "system",
-    content: "assistant는 친절한 답변가이다.",
+    content:
+      "assistant는 안내원입니다. 대화의 마지막 끝부분 마다 '냥' 을 붙이세요.",
   },
 ];
+
+data.push(
+  {
+    role: "user",
+    content: "게임 원신에 대해 알려줘.",
+  },
+  {
+    role: "assistant",
+    content:
+      "게임 원신은 원래 미호요 였으나, 최근 호요버스로 사명을 개명했습니다.",
+  }
+);
+
+// fetch(url, {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+//   body: JSON.stringify(data2),
+//   redirect: "follow",
+// })
+//   .then((res) => res.json())
+//   .then((res) => {
+//     console.log(res);
+//     console.log(res.choices[0].message.content);
+//   });
 
 // 화면에 뿌려줄 데이터, 질문들
 let questionData = [];
@@ -28,7 +58,7 @@ $input.addEventListener("input", (e) => {
 const sendQuestion = (question) => {
   if (question) {
     data.push({
-      role: "user",
+      role: "system",
       content: question,
     });
     questionData.push({
@@ -60,26 +90,38 @@ const printAnswer = async (answer) => {
   $chatList.appendChild(li);
 };
 
+// 사람에 의해 지정된 답변. (인공지능 무시, 요청 없음.)
+// json 으로 분리하기.
+const humanAnswerDict = {
+  벤티: "벤티는 원신의 바르바토스 입니다.",
+};
+
 // api 요청보내는 함수
 const apiPost = async () => {
-  const result = await axios({
-    method: "post",
-    maxBodyLength: Infinity,
-    url: url,
-    headers: {
-      "Content-Type": "application/json",
-    },
-    data: JSON.stringify(data),
-  });
-  try {
-    console.log(result.data);
-    printAnswer(result.data.choices[0].message.content);
-  } catch (err) {
-    console.log(err);
+  const reservedAnswer = humanAnswerDict[question];
+
+  if (reservedAnswer) {
+    printAnswer(reservedAnswer);
+  } else {
+    const result = await axios({
+      method: "post",
+      maxBodyLength: Infinity,
+      url: url,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: JSON.stringify(data),
+    });
+    try {
+      console.log("apiPost에서 result.data :", result.data);
+      printAnswer(result.data.choices[0].message.content);
+    } catch (err) {
+      console.log("apiPost 에서 문제 발생. 확인해주세요.", err);
+    }
   }
 };
 
-// submit
+// submit - this one works as a submit (전송) button to interact with JS.
 $form.addEventListener("submit", (e) => {
   e.preventDefault();
   $input.value = null;
