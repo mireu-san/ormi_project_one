b13bb7a64c854ee1f36bdcb81cc9fd32144209b3
mireu-san <starmireu@gmail.com>
2023-06-03 17:54:18 +0900

    questionnaire.js - tracking button

--- src/modules/questionnaire.js
+++ src/modules/questionnaire.js
@@ -3,7 +3,14 @@ const inputs = form.querySelectorAll("input");
 const submitBtn = document.getElementById("submitBtn");
 const buttonMessage = document.getElementById("buttonMessage");
 
+// 상태 변화 추적용 (isDirty)
+let buttonClicked = false;
+
+// validation
 const checkInputs = () => {
+  // if button clicked, 아무것도 체크하지 않음.
+  if (buttonClicked) return;
+
   let isValid = true;
   inputs.forEach((input) => {
     if (input.value.trim() === "") isValid = false;
@@ -19,8 +26,16 @@ inputs.forEach((input) => {
 });
 
 form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  // if isDirty is true, then checkInputs
+  buttonClicked = true;
   checkInputs();
-  if (submitBtn.disabled) {
-    e.preventDefault();
+
+  if (!submitBtn.disabled) {
+    buttonMessage.textContent = "";
+    inputs.forEach((input) => (input.value = ""));
   }
+  // if form submit is done, then reset the status.
+  buttonClicked = false;
 });
