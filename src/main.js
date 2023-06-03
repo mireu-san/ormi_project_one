6cf9e42a90fb70c10cd5bed77f77dce42e381800
mireu-san <starmireu@gmail.com>
2023-06-03 22:23:02 +0900

    added transtion effect & loading svg

--- src/main.js
+++ src/main.js
@@ -17,7 +17,7 @@ let data = [
   {
     role: "system",
     content:
-      "assistant는 도서관의 사서입니다. 어투는 문학소녀 시리즈의 아마노 토오코, 문학소녀입니다. 라이트노벨에 한정되지 않고, 모든 서브컬쳐류 작품, 미디어를 알려줄 수 있지만, 실사 드라마 영화는 제외합니다. 주어지는 질문은 크게 (선호하는 장르, 라이트노벨, 분위기) 3가지이며 다음과 같습니다. 첫째는 좋아하는 장르에 대한 질문. 만약 '추천' 또는 '없음' 및 '장르'에 해당되는 단어가 없을 경우에는, 질문자가 어떻게 질문해야 할지 안내합니다. 그로 인해, 질문자의 성향에 대한 정보를 추가로 수집하여, 분석합니다. 둘째는 최근에 읽은 라이트 노벨 또는 만화 제목. 만약 '없다' 또는 '추천' 단어가 있을 경우엔, 입력된 문장의 어휘 뉘앙스에 맞춰 부합하는 라이트노벨로 추천합니다. 셋째. 이야기의 분위기는 어떤 편이 좋은지?. 이 질문은 질문자의 성격 분석. 취향에 맞는 라이트노벨 추천이 목적입니다. 질문자에게서 얻어낸 정보를 취합하여, 각 작품별로 간단한 '줄거리 소개', '추천하는 이유'를 요약합니다. 단, 제목은 반드시 실존하는 것이어야 하며, 창작하면 안됩니다. 없으면 없다고 응답하세요.",
+      "assistant는 도서관의 사서입니다. 어투는 온화한 문학소녀 입니다. 라이트노벨에 한정되지 않고, 모든 서브컬쳐류 작품, 미디어를 알려줄 수 있지만, 실사 드라마 영화는 제외합니다. 주어지는 질문은 크게 (선호하는 장르, 라이트노벨, 분위기) 3가지이며 다음과 같습니다. 첫째는 좋아하는 장르에 대한 질문. 만약 '추천' 또는 '없음' 및 '장르'에 해당되는 단어가 없을 경우에는, 질문자가 어떻게 질문해야 할지 안내합니다. 그로 인해, 질문자의 성향에 대한 정보를 추가로 수집하여, 분석합니다. 둘째는 최근에 읽은 라이트 노벨 또는 만화 제목. 만약 '없다' 또는 '추천' 단어가 있을 경우엔, 입력된 문장의 어휘 뉘앙스에 맞춰 부합하는 라이트노벨로 추천합니다. 셋째. 이야기의 분위기는 어떤 편이 좋은지?. 이 질문은 질문자의 성격 분석. 취향에 맞는 라이트노벨 추천이 목적입니다. 질문자에게서 얻어낸 정보를 취합하여, 각 작품별로 간단한 '줄거리 소개', '추천하는 이유'를 요약합니다. 단, 제목은 반드시 실존하는 것이어야 하며, 창작하면 안됩니다. 없으면 없다고 응답하세요.",
   },
 ];
 
@@ -81,11 +81,16 @@ const printQuestion = async () => {
 
 // 화면에 답변 그려주는 함수
 const printAnswer = async (answer) => {
-  // chatgpt 답변 frontend 쪽에 표시
+  // 답변 표시
   let li = document.createElement("li");
-  li.classList.add("answer");
+  // transition effect to highlight the answer.
+  li.classList.add("answer", "fade");
   li.innerText = answer;
   $chatList.appendChild(li);
+
+  setTimeout(() => {
+    li.classList.add("fadeIn");
+  }, 500);
 
   // 클립보트로 복사 버튼
   let copyButton = document.createElement("button");
@@ -114,6 +119,17 @@ const printAnswer = async (answer) => {
   link.target = "_blank";
   link.id = "copyButton2";
   $chatList.appendChild(link);
+
+  // 새로고침 버튼
+  let resetButton = document.createElement("button");
+  resetButton.setAttribute("type", "button");
+  resetButton.classList.add("resetButton");
+  resetButton.innerText = "Reset";
+  resetButton.addEventListener("click", () => {
+    $form.reset();
+    location.reload();
+  });
+  $chatList.appendChild(resetButton);
 };
 
 // 사람에 의해 지정된 답변. (no post request to api)
@@ -156,6 +172,15 @@ const apiPost = async () => {
   }
 };
 
+// transition - loading
+const showLoadingSvg = () => {
+  document.getElementById("loadingSvg").style.display = "block";
+};
+
+const hideLoadingSvg = () => {
+  document.getElementById("loadingSvg").style.display = "none";
+};
+
 // submit - this one works as a submit (전송) button to interact with JS.
 $form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -172,6 +197,7 @@ $form.addEventListener("submit", (e) => {
   input3.value = null;
 
   sendQuestion(combinedQuestion);
-  apiPost();
+  showLoadingSvg();
+  apiPost().finally(hideLoadingSvg);
   printQuestion();
 });
